f8f9ead1d39d16c8c0d7687d74b4734f3de28256
simiedeki <164128583+simiedeki@users.noreply.github.com>
2024-05-04 05:50:14 +0000

    started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,49 @@
 class UsersController < ApplicationController
-
   def index
-    matching_users = User.all
-    @users = matching_users.order(:created_at)
-
-    render({ :template => "users_templates/all_users"})
+    matching_users=User.all
+    @list_of_users = matching_users.order({:username => :asc})
+    render "users/index"
   end
-  
   def show
-    username = params.fetch("username")
-    matching_users = User.where({ :username => username })
-    @user = matching_users.at(0)
+    url_username = params.fetch("username")
 
-    render({ :template => "user_templates/user_details"})
+    matching_usernames = User.where({ :username => url_username })
+    @the_user = matching_usernames.first
+    if @the_user.nil?
+      redirect_to users_path, alert: "User not found."
+      return
+    end
+      
+    render({ :template => "users/show" })
   end
-  
   def create
-    user = User.new
-    user.username = params.fetch("query_username")
-    user.save
-    
-    redirect_to("/users/#{user.username}")
-  end
-  
-  def update
-    user_id = params.fetch("user_id")
-    matching_users = User.where({ :id => user_id })
-    the_user = matching_users.at(0)
-    
-    the_user.username = params.fetch("query_username")
-    the_user.save
-    redirect_to("/users/#{user.username}")
-  end
+    a = User.new
+    a.username = params.fetch("input_username")
+    a.save
 
+    redirect_to "/users/#{a.username}"
+  end
+  def update
+    the_id = params.fetch("path_id")
+    @the_user = User.where({ :id => the_id }).at(0)
+
+    @the_user.username = params.fetch("query_username")
+
+
+    if @the_user.valid?
+      @the_user.save
+      redirect_to("/users/#{@the_user.id}", { :notice => "user updated successfully."} )
+    else
+      redirect_to("/users/#{@the_user.id}", { :alert => "user failed to update successfully." })
+    end
+  end
+   
+  
+    
+
+
+  
+
+  
+   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "users", :action => "home" })
-  get("/users", { :controller => "users", :action => "all_users" })
-  get("/users/:username", { :controller => "user", :action => "show" })
-  post("/insert_user_record", { :controller => "venues", :action => "create" })
-  post("/update_users/:user_id", { :controller => "users", :action => "update" })
-
-  get("/venues", { :controller => "venues", :action => "index" })
-  post("/insert_venue_record", { :controller => "venues", :action => "create" })
-  get("/venue/:an_id", { :controller => "venue", :action => "show" })
-  post("/update_venue/:the_id", { :controller => "application", :action => "update" })
-  get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
-  
-  post("/insert_comment_record", { :controller => "comments", :action => "create" })
+get '/users', to: 'users#index'
+get '/users/:username', to: 'users#show'
+get '/', to: 'users#index'
+get '/venues', to: 'venues#index'
+get '/venues/:path_id', to: 'venues#show'
+get '/delete_venue/:path_id', to: 'venues#delete'
+get '/delete_venue/:path_id', to: 'venues#delete'
+post("/add_user", { :controller => "users", :action => "create" })
+post("/insert_venue_record", { :controller => "venues", :action => "create" })
+post("/update_user/:path_id", { :controller => "users", :action => "update" })
+post("/update_venue/:path_id", { :controller => "venues", :action => "update" })
 end
+
